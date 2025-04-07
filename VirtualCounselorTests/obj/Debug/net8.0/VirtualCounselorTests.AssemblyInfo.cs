8e7f650803ce7c68163580efcedf09b982d7809b
StaticSnap <tylerrcox@outlook.com>
2025-04-06 22:26:41 -0700

    end of sprint 2

--- VirtualCounselorTests/obj/Debug/net8.0/VirtualCounselorTests.AssemblyInfo.cs
+++ VirtualCounselorTests/obj/Debug/net8.0/VirtualCounselorTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtualCounselorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c3b6919a595da01f8ee8841e652c65db281e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f71e1f6ce3e9f0306bf445d8fa4d0810ac1fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtualCounselorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtualCounselorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
