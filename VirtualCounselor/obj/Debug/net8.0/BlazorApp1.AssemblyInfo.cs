3555a97ad1f4b1942d2103ef18e91cf77c481312
StaticSnap <tylerrcox@outlook.com>
2025-04-25 10:04:07 -0700

    navbar redesign

--- VirtualCounselor/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ VirtualCounselor/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d072d59221dbb83991af7d94fbef48f96388cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875af2e656ae3b0e31aa7165570da69cdaf93038")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
